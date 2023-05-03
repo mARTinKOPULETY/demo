11892ede093e07e37003e17ec32440f9c49c490d
mARTinKOPULETY <kopuletym@seznam.cz>
2023-05-03 17:55:13 +0200

    * api/v1/snakes - udpated SnakeController, app works well, fundamental CRUD operations, without validation of content.

--- src/main/java/cz/martinkopulety/demo/controller/SnakeController.java
+++ src/main/java/cz/martinkopulety/demo/controller/SnakeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/snakes")
+@RequestMapping("/api/v1/snakes")
 public class SnakeController {
     @Autowired
     private SnakeService snakeService;
