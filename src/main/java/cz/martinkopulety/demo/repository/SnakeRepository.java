9b624be6dfbeb44d4eadf5bd41fa5d254ec365df
mARTinKOPULETY <kopuletym@seznam.cz>
2023-05-10 12:41:05 +0200

    * SnakeRepository.java - @Query was added to distinguish upper and lower case letters for methods: findBySnakePicContaining() and findBySnakeNameContaining().

--- src/main/java/cz/martinkopulety/demo/repository/SnakeRepository.java
+++ src/main/java/cz/martinkopulety/demo/repository/SnakeRepository.java
@@ -2,12 +2,17 @@ package cz.martinkopulety.demo.repository;
 
 import cz.martinkopulety.demo.entity.Snake;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface SnakeRepository extends JpaRepository<Snake, Long> {
-    List<Snake> findBySnakePicContaining(String partOfSnakePic);
+    @Query(value = "SELECT  *  FROM snake  WHERE snake_pic LIKE BINARY CONCAT('%',:partOfSnakePic,'%')", nativeQuery = true)
+    List<Snake> findBySnakePicContaining(@Param("partOfSnakePic") String partOfSnakePic);
+
+    @Query(value = "SELECT  *  FROM snake  WHERE snake_name LIKE BINARY CONCAT('%',:partOfSnakeName,'%')", nativeQuery = true)
     List<Snake> findBySnakeNameContaining(String partOfSnakeName);
 }
